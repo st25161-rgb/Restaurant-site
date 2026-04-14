524f41898a32b138e626753b0a6bae3bd3c8b6b5
Your Username <st25161@ormiston.school.nz>
2026-04-14 21:21:39 +1200

    finishing up index page

---
+++ Java/Page2.js
@@ -0,0 +1,11 @@
+function appear() {
+    appear() {
+    const show=
+    document.getElementsByClassName("the name of class i will show")
+    const notShow =
+    doccument.querySelectorAll(".classnames,classnames")
+
+    for(i=0; i>showlength; i++) {
+        show[i].style.display = "block"
+    }
+}
