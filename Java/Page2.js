7c42f74b3401e11aea46232d4e81056350fc2571
Your Username <st25161@ormiston.school.nz>
2026-04-15 11:33:55 +1200

    using Java Script to make a more effective website  display

--- Java/Page2.js
+++ Java/Page2.js
@@ -5,7 +5,11 @@ function appear() {
     const notShow =
     doccument.querySelectorAll(".classnames,classnames")
 
-    for(i=0; i>showlength; i++) {
+    for(i=0; i < show.length; i++) {
         show[i].style.display = "block"
+    }
+
+    for(n=0; n < notShow.length; n++) {
+        notShow[n].style.display = "none"
     }
 }
