6939b1fa128301e09fed0124e41ee46e246689fe
Your Username <st25161@ormiston.school.nz>
2026-04-16 16:29:13 +1200

    styling text of second page will add images after

--- Java/Page2.js
+++ Java/Page2.js
@@ -1,15 +1,16 @@
-function appear() {
-    appear() {
-    const show=
-    document.getElementsByClassName("cuisines")
-    const notShow =
-    doccument.querySelectorAll(".Thai, Italian")
+// function appear(evt, cuisines) {
+//     var i, cusine
+//     appear() {
+//     const show=
+//     document.getElementsByClassName("cuisines")
+//     const notShow =
+//     doccument.querySelectorAll(".Thai, Italian")
 
-    for(i=0; i < show.length; i++) {
-        show[i].style.display = "block"
-    }
+//     for(i=0; i < show.length; i++) {
+//         show[i].style.display = "block"
+//     }
 
-    for(n=0; n < notShow.length; n++) {
-        notShow[n].style.display = "none"
-    }
-}
+//     for(n=0; n < notShow.length; n++) {
+//         notShow[n].style.display = "none"
+//     }
+// }
